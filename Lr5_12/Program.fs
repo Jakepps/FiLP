026b4c0cdcf48af9fba2476ad1797cfd3bc6d86c
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-03-18 22:24:03 +0300

    Add files via upload

---
+++ Lr5_12/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+ 
+ [<EntryPoint>]
+ let main argv =
+     let vopr (lang:string)=
+         match lang.Trim() with
+         |"F#"|"Prolog" -> "Подлиза"
+         |"C++" -> "попущ"
+         |"Python"->"гиренко в школе"
+         |"C#" -> "боже выйди"
+         |"Pascal" -> "когда на утренник ?"
+         |_ ->"ясно биофак" 
+
+     //Суперпозиция
+     Console.WriteLine("Какой твой люимый язык программирования?")
+     (Console.ReadLine >> vopr >> Console.WriteLine)()
+
+    //Каррирование
+     Console.WriteLine("Какой твой люимый язык программирования?")
+     let result vopr func out = out(func(vopr()))
+     result Console.ReadLine vopr Console.WriteLine
+
+     0 
