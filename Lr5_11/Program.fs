fa93e8b6cdc6f21c8bc39500a6a83d8d809c41cc
Jakepps <96076243+Jakepps@users.noreply.github.com>
2022-03-18 22:12:04 +0300

    Add files via upload

---
+++ Lr5_11/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let vopr q = 
+    match q with
+        |"F#" | "Prolog" -> printfn "Подлиза"
+        |other -> printfn "%s"("Кринж")
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой люимый язык программирования?"
+    let c = Console.ReadLine()
+    vopr c
+    0
