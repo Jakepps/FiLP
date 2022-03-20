bb75596f4c6e59434fd693457a0abd2afce7c2f7
Jakepps <nagalevsiy@mail.ru>
2022-03-20 17:13:00 +0300

    5.20-реал. использование функ. из других проектов

---
+++ Lr5_20/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let ChoiceFunction = function
+    | 1 -> Program.Eyler
+    | 2 -> Program.Sum3
+    | _ -> Program.MaxPrNum 
+
+[<EntryPoint>]
+let main argv =
+
+    Console.WriteLine("Введите номер функции и число: ")
+    let data = (Console.ReadLine() |> Int32.Parse, Console.ReadLine() |> Int32.Parse)
+    let result = ChoiceFunction (fst data) (snd data)
+    printfn "Ответ: %d"  result   
+    0
