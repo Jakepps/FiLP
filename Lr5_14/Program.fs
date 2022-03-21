2e3d22138a910fe0d8cf23077c8c8fa39caaac09
Jakepps <nagalevsiy@mail.ru>
2022-03-21 14:13:22 +0300

    Перезалил 5.14,почему то гит залил дефолтную прогу

--- Lr5_14/Program.fs
+++ Lr5_14/Program.fs
@@ -1,13 +1,19 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿open System 
 
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let Del n f init=
+    let rec FDel n f init newN=
+        if newN=0 then init
+        else 
+            let NextInit=
+                if n%newN=0 then f init newN
+                else init
+            let supernewN=newN-1
+            FDel n f NextInit supernewN
+    FDel n f init n
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main c= 
+    Console.WriteLine("Ввидите число:")
+    let n = Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine(Del n (fun x y -> x+y) 1)
+    0
