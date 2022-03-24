878b8e622dce1141267a75778524206f7194a1b0
Jakepps <nagalevsiy@mail.ru>
2022-03-24 22:41:46 +0300

    6.38 почему последние задания легче предыдущих

---
+++ Lr6_38/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let CountOnAB list a b = 
+    let rec CountOnAB1 list a b count = 
+        match list with 
+        |[]-> count
+        |head::tail -> if head >= a && head <= b then CountOnAB1 tail a b count+1
+                               else CountOnAB1 tail a b count
+    CountOnAB1 list a b 0
+
+[<EntryPoint>]
+let main argv =
+    let list = Program.readData
+    Console.WriteLine(CountOnAB list 1 10)
+
+    0
