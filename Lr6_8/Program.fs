1a51cf91a8941f74f8d8fc3161cb24e519a96bf6
Jakepps <nagalevsiy@mail.ru>
2022-03-24 20:00:55 +0300

    6.8

---
+++ Lr6_8/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let Min list = 
+    let rec Min2 list min1 min2 index1 index2 currentIndex = 
+        match list with 
+        |[] -> (index1, index2)
+        |head::tail-> if (head <= min1 && head >= min2 && currentIndex <> index1) then 
+                                        Min2 tail head min2 currentIndex index2 (currentIndex+1)
+                      elif (head >= min1 && head <= min2 && currentIndex <> index2) then 
+                                        Min2 tail min1 head index1 currentIndex (currentIndex+1)
+                      elif (head <= min1 && head <= min2)  then 
+                                        if min1 <= min2 then Min2 tail min1 head index1 currentIndex (currentIndex+1)
+                                        else Min2 tail head min2 currentIndex index2 (currentIndex+1)
+                      else
+                                        Min2 tail min1 min2 index1 index2 (currentIndex+1)
+    Min2 list list.[0] list.[1] 0 1 0
+
+[<EntryPoint>]
+let main argv =
+    let list = Program.readData
+    Program.writeList list
+    Console.WriteLine(Min list)   
+    //4 5 1 2 3 ->(2,3)
+    0
