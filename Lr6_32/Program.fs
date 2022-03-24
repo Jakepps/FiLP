07b1ce3f98b753439d154f8f0042fb84ed35246a
Jakepps <nagalevsiy@mail.ru>
2022-03-24 21:24:51 +0300

    6.32,узнал что такое лок. максимум в массиве ого!

---
+++ Lr6_32/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let LocalMax list = 
+    let rec LocalMax1 list maxElem count = 
+        match list with
+        []-> count
+        |head :: tail -> if head = maxElem then LocalMax1 tail maxElem  (count+1)
+                         else LocalMax1 tail maxElem count
+    LocalMax1 list (fst(Program.Fmax list)) 0 
+
+[<EntryPoint>]
+let main argv =
+    let list = Program.readData 
+    Console.WriteLine(LocalMax list)
+    
+    0
