2c2ecbc7cf4ae079ff9e12a19ce67ff3e9c72006
Jakepps <nagalevsiy@mail.ru>
2022-03-24 19:39:34 +0300

    6.7 с использованием некоторых функций из 6.11

---
+++ Lr6_7/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+//удаление элемента из списка с номером n
+let DelElem list n = 
+    let rec DelElem1 (list: 'a list) n (list1: 'a list) currentIndex = 
+        if n = currentIndex then list1 @ list.Tail
+        else
+            let newList1 = list1 @ [list.Head]
+            DelElem1 list.Tail n newList1 (currentIndex+1)
+    DelElem1 list n [] 0
+
+
+let rec ShiftListRight (list: 'a list) n =
+    if n=0 then list
+    else
+        if list.Length <= 1 then list 
+        else
+           ShiftListRight ([list.[list.Length-1]] @ DelElem list (list.Length-1)) (n-1) // список, полученный путём list[last]+list без последнего эл-та
+
+[<EntryPoint>]
+let main argv =
+    let list = Program.readData
+    Program.writeList(ShiftListRight list 2)
+    //1 2 3 4 5 ->4 5 1 2 3 
+    0
