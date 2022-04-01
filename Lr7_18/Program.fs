df6c4b78c2bd50bb8fb4fbc0487cd1ddd5f3bbb4
Jakepps <nagalevsiy@mail.ru>
2022-04-01 20:43:45 +0300

    7.18

--- Lr7_18/Program.fs
+++ Lr7_18/Program.fs
@@ -1,25 +1,10 @@
 ﻿open System
 
-//элементы до N 
-//append - объединить два списка в один
-let ElemBeforeMin list = 
-    let rec ElemBeforeMin1 list currentIndex minIndex resultList = 
-        match list with
-        []->resultList
-        |head :: tail -> 
-                         if (currentIndex < minIndex) then 
-                            ElemBeforeMin1 tail (currentIndex+1) minIndex (List.append resultList [head])
-                         else resultList
-                         
-    ElemBeforeMin1 list 0  (List.findIndex (fun x->x = (List.min list)) list) []
-
-
+let trimListByLastMin list =
+    List.init (List.findIndex (fun x -> x = List.min list) list) (fun index -> List.item index list)
 
 [<EntryPoint>]
 let main argv =
-    let list = Program.readData 
-    //Program.writeList list 
-    let a = ElemBeforeMin list
-    Program.writeList a
-    //1 2 3 -5 4 ->1 2 3
+    let list = Program.readData
+    Program.writeList (trimListByLastMin list)
     0
