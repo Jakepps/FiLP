4e167c116bbf8f9f0f24662607ccbd0fdac010a9
Jakepps <nagalevsiy@mail.ru>
2022-04-01 17:54:33 +0300

    7.38

---
+++ Lr7_38/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+//количество элементов на  отрезке а б 
+let CountElemOnAB list a b = 
+    let rec CountElemOnAB1 list a b resultList count= 
+        match list with
+        []-> resultList
+        |head :: tail -> if head >= a && head <= b then 
+                                                    CountElemOnAB1 tail a b (List.append resultList [head]) (count+1)
+                         else CountElemOnAB1 tail a b resultList count 
+    CountElemOnAB1 list a b [] 0   
+    
+
+[<EntryPoint>]
+let main argv =
+    
+    let list=Program.readData
+    let a=CountElemOnAB list 1 10
+    Console.WriteLine a
+
+    0
