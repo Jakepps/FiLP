cb08e45a2111853a60fde10d6549f0d2454f3949
Jakepps <nagalevsiy@mail.ru>
2022-04-01 21:11:11 +0300

    7.28

--- Lr7_28/Program.fs
+++ Lr7_28/Program.fs
@@ -1,21 +1,10 @@
 ﻿open System
 
-//элементы между первым и последним максимальным
-let ElemBetweenFirstLastMax list= 
-    let rec ElemBetweenFirstLastMax1 list maxIndex resultList = 
-        match list with    
-        [] -> resultList
-        |head :: a :: tail -> if maxIndex <> 1 then 
-                                            ElemBetweenFirstLastMax1 (Program.remove 1 list) (maxIndex-1) (List.append resultList [a])
-                                            //maxIndex-1 так как из list удалился элемент до него, т.е. он сместился влево
-                              else resultList
-        |head :: tail -> resultList
-    ElemBetweenFirstLastMax1 list (List.findIndexBack (fun x-> x = (List.max list)) list) []
-
 [<EntryPoint>]
 let main argv =
-    
-    let list= Program.readData
-    Program.writeList (ElemBetweenFirstLastMax list)
-    
-    0
+    let list = Program.readData
+    let new_l = List.filter (fun x -> x < List.max list && x > List.min list) list
+    let ll = List.max new_l
+    Console.WriteLine ll
+
+    0 
