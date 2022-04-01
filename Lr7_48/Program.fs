ad2e8e83ef777521276c49ab44725c7b2d333329
Jakepps <nagalevsiy@mail.ru>
2022-04-01 22:44:17 +0300

    7.58-было в 0 варианте колока-решал

--- Lr7_48/Program.fs
+++ Lr7_48/Program.fs
@@ -4,7 +4,7 @@ let rec readList n =
     List.init(n) (fun index->Console.ReadLine()|>Convert.ToDouble)
 
 let readData = 
-    let n=Convert.ToInt32(System.Console.ReadLine())
+    let n=Convert.ToInt32(Console.ReadLine())
     readList n
 
 let AverageAbsList list = List.average (List.map (fun (x:float)-> Math.Abs(x)) list )
---
+++ Lr7_58/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+let sortByCount list = List.sortBy(fun x -> -(List.filter(fun v -> x=v) list).Length) list
+
+[<EntryPoint>]
+let main argv =
+   let l = Program.readData
+   sortByCount l |> Program.writeList
+   
+   0
