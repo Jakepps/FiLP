2e3594d6f0bc04aa6699ba9be946f1fbfda29946
Jakepps <nagalevsiy@mail.ru>
2022-04-02 10:21:02 +0300

    1 метод в 7.19.2

---
+++ lr7_19_2/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+let Orderlines str =
+    let justLower = String.filter (fun x -> Char.IsLower x) str
+    let rec isOrder (str: string) ind =
+        if (ind = str.Length-1) then true
+        else
+            if (str.[ind] <= str.[ind+1]) then isOrder str (ind+1)
+            else false
+    isOrder justLower 0
+
+
+
+let choose n str =
+    match n with
+    | 1 -> printfn "%b" (Orderlines str)
+//    | 2 -> printfn "%i" (provA str)
+ //   | 3-> printfn "%s" (namefile str)
+    |_-> printfn "%A" ("Cringe")
+
+[<EntryPoint>]
+let main argv =
+     printfn "%A" "Введите строку: " 
+     let str = Convert.ToString(Console.ReadLine())
+     printfn "%A" "Введите номер задачи: " 
+     let n = Convert.ToInt32(Console.ReadLine())
+     choose n str   
+    
+     0
