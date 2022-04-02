cecf248fe3ed41ea357b10d9de65befe78a8ff11
Jakepps <nagalevsiy@mail.ru>
2022-04-02 10:29:27 +0300

    3 метод 7.19

--- lr7_19_2/Program.fs
+++ lr7_19_2/Program.fs
@@ -12,13 +12,25 @@ let provA str=
     let prov2 = String.filter(fun x-> x='A') str
     prov2.Length
 
-
+let namefile str =
+    let rec takeBackWhile (str: string) separator ind =
+        if (ind = -1) then str
+        else
+            if (str.[ind] = separator) then str.[(ind+1)..]
+            else takeBackWhile str separator (ind-1)
+    let rec takeWhile (str: string) separator ind =
+        if (ind = str.Length) then str
+        else
+            if (str.[ind] = separator) then str.[..(ind-1)]
+            else takeWhile str separator (ind+1)
+    let justFile = takeBackWhile str '\\' (str.Length-1)
+    takeWhile justFile '.' 0
 
 let choose n str =
     match n with
     | 1 -> printfn "%b" (Orderlines str)
     | 2 -> printfn "%i" (provA str)
- //   | 3-> printfn "%s" (namefile str)
+    | 3-> printfn "%s" (namefile str)
     |_-> printfn "%A" ("Cringe")
 
 [<EntryPoint>]
