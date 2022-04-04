d27a5eebfb395799cad7c392aac03d3a3ebc4505
Jakepps <nagalevsiy@mail.ru>
2022-04-04 19:06:02 +0300

    c горем пополам 7.20

--- lr7_20/Program.fs
+++ lr7_20/Program.fs
@@ -9,24 +9,7 @@ let writeArray arr =
             realWriteArray arr nextind
     realWriteArray arr 0
 
-let ascii char = int char - int '0'
 
-let stringFold (func: 'state -> char -> 'state) (init: 'state) (value: string) =
-    let rec _stringFold (index: int) (state: 'state) =
-        match index with
-        | final when final = value.Length -> state
-        | _ -> 
-            let newState = func state (value.Chars index)
-            let newIndex = index + 1
-            _stringFold newIndex newState
-    _stringFold 0 init
-
-let asciiWeight string = (stringFold (fun sum v -> sum + float (ascii v)) 0.0 string) / (float string.Length)
-
-let getMaxWeight (string: string) = 
-    stringFold (fun state va ->
-        if ascii (fst state) + (ascii (snd state)) + (ascii (fst (snd state))) then state else state
-    ) (string.Chars 0; 0) string
 //В порядке увеличения среднего веса ASCII-кода символа строки
 let metod1 arrStr =
     let rec foundAvgOfKode (str:string) ind (acc:int)=
@@ -40,15 +23,37 @@ let metod1 arrStr =
         arrStr
 
 
+//В порядке увеличения квадратичного отклонения между наиболь-
+//шим ASCII-кодом символа строки и разницы в ASCII-кодах пар зеркально
+//расположенных символов строки (относительно ее середины)
+let maxASCII (str:string) =
+    let rec maxASCIIInternal index (count:int) =
+        match index with
+        | index when index >= str.Length - 1 -> count
+        |_ ->
+            let newCount = if (str.[index] |> Convert.ToInt32)  > count then (str.[index] |> Convert.ToInt32) else count
+            let newIndex = index + 1
+            maxASCIIInternal newIndex newCount
+    maxASCIIInternal 0 ('A' |> Convert.ToInt32)
 
+let sumRazn (str:string) =
+    let rec sumRazn2 index index2 count =
+        match index with
+        | index when index2 <= index -> count
+        |_ ->
+            let newCount = (str.[index] |> Convert.ToInt32) - (str.[index2] |> Convert.ToInt32) + count
+            let newIndex = index + 1
+            let newIndex2 = index2 - 1
+            sumRazn2 newIndex  newIndex2 newCount
+    sumRazn2 0 ((str.Length) - 1) 0
 
-
-
+let metod2 list =
+    List.sortBy (fun x -> (maxASCII x - sumRazn x)*(maxASCII x - sumRazn x) ) list
 
 let multimetod n str =
     match n with
     |1 -> writeArray (metod1 str)
- //   |2-> writeArray (metod2 str)
+    |2-> writeArray (metod2 str)
     |_->printfn "%A" ("cringe")
 
 [<EntryPoint>]
