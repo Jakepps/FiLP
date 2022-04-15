203c29151fe349fb5219554633456874d98602f1
Jakepps <nagalevsiy@mail.ru>
2022-04-15 23:19:37 +0300

    очень остроумное 8.4

---
+++ Lr8_4/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let printerAgent = MailboxProcessor.Start(fun inbox->
+    let rec messageLoop() = async{
+        let! msg = inbox.Receive()
+        
+        match msg with 
+        |"Вилка" -> printfn "%A" ("Я не видел одноглазых програмистов")
+        |"Ложка" -> printfn "%A" ("так,это уже не канон")
+        |other ->printf "%A" ("Мы ограничены цензурой")
+        return! messageLoop()
+        }
+    messageLoop()
+
+    )
+[<EntryPoint>]
+let main argv =
+    let a1=printerAgent.Post("Вилка")
+    let a2=printerAgent.Post("Ложка")
+    let a3=printerAgent.Post("F#")
+    Console.ReadKey()
+    0 // return an integer exit code
